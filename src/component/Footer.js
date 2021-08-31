29e583f011b2fa162199d4ff17a9a87a93a938f4
samahhamed227 <samahhamed227@gmail.com>
2021-08-31 15:17:46 +0300

    edit

---
+++ src/component/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <h5>Samah Hamed</h5>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
