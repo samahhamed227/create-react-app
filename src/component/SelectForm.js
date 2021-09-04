3452d60e919fd4c326558b6dcb96553f9a22501d
samahhamed227 <samahhamed227@gmail.com>
2021-09-04 16:37:11 +0300

    DoneAll

--- src/component/SelectForm.js
+++ src/component/SelectForm.js
@@ -10,7 +10,7 @@ class SelectForm extends React.Component {
     let NumHorn = parseInt(event.target.value);
     let FilterData = data.filter((i) => {
       if (i.horns === NumHorn) {
-        return true;
+        return NumHorn;
       } else if (NumHorn === 0) {
         return data;
       }
@@ -25,11 +25,11 @@ class SelectForm extends React.Component {
           <Form.Group controlId="exampleForm.SelectCustom">
             <Form.Label>Number Of Horns</Form.Label>
             <Form.Control as="select" custom onChange={this.filtered}>
-              <option>All</option>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>100</option>
+              <option value="0">All</option>
+              <option value="1">1</option>
+              <option  value="2">2</option>
+              <option  value="3">3</option>
+              <option  value="100">100</option>
             </Form.Control>
           </Form.Group>
         </Form>
